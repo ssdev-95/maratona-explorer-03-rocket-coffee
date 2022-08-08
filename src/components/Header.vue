e01b54305c8f5456c2e76e6ef77d0d6d7f8a2126
Salomao S <souza95salomao@gmail.com>
2022-08-08 00:50:39 +0000

    update: style basis and header component

---
+++ src/components/Header.vue
@@ -0,0 +1,32 @@
+<template>
+  <header>
+	  <h1>Rocket<span>Coffee</span></h1>
+		<p>Open everyday, 8am ~ 9pm</p>
+	</header>
+</template>
+
+<style scoped lang="css">
+  header {
+	  width: 100vw;
+		padding: 2.75rem 0.75rem;
+
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		gap: 0.5rem;
+	}
+
+	header > h1 {
+	  color: #1c1c1c;
+		text-transform: uppercase;
+	}
+
+	header > h1 > span {
+	  color: var(--brown);
+	}
+
+	header > p {
+	  color: var(--gray);
+	}
+</style>
